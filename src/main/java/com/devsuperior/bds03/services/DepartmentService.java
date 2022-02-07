504b3e4a62a039cd93cc7efa9fdb40923adc9798
Alan Pacheco <alan.estudo@gmail.com>
2022-02-07 01:35:48 -0300

    Exercise completed successfully

--- src/main/java/com/devsuperior/bds03/services/DepartmentService.java
+++ src/main/java/com/devsuperior/bds03/services/DepartmentService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.bds03.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.bds03.dto.DepartmentDTO;
 import com.devsuperior.bds03.entities.Department;
 import com.devsuperior.bds03.repositories.DepartmentRepository;
+import com.devsuperior.bds03.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class DepartmentService {
@@ -20,5 +22,12 @@ public class DepartmentService {
 	public List<DepartmentDTO> findAll() {
 		List<Department> list = repository.findAll(Sort.by("name"));
 		return list.stream().map(x -> new DepartmentDTO(x)).collect(Collectors.toList());
+	}
+	
+	public DepartmentDTO findById(Long id) {
+		Optional<Department> optional = repository.findById(id);
+		Department department = optional.orElseThrow(
+				()-> new ResourceNotFoundException("id not found: " + id));
+		return new DepartmentDTO(department);
 	}
 }
